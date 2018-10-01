2234a6c5fe71b492448a97352544c252c817816a
kcluniesa <kcluniesa@yahoo.com>
2018-10-01 23:37:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,7 +108,7 @@ def play(board)
     counter += 1
   end  
     if !!won?(board)
-      puts "Congratulations you won"
+      puts "Congratulations X!"
     elsif draw?(board) == true
       puts "Cat's Game!"
     else
